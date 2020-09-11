d398f79b287692de82f5b64efab654bb00fa783b
Quentin Sar <sarquentin@gmail.com>
2020-09-11 19:56:24 +0100

    Update

--- resources/views/sections/services/brand-2.blade.php
+++ resources/views/sections/services/brand-2.blade.php
@@ -8,26 +8,26 @@
                     <img itemprop="logo" src="{{asset('images/pres/dev.png')}}">
                 </div>
                 <div class="title" itemprop="slogan">
-                    <h2>Créons ensemble</h2>
-                    <h3>Le design de vos rêves</h3>
+                    <h2>Notre équipe</h2>
+                    <h3>Développe votre site</h3>
                 </div>
             </div>
             <div class="txt">
                 <div class="description" itemprop="description">
-                    <p>Le design est une partie essentielle du site, il représente sa façade, la première impression que l'on a du site. Nous vous apportons notre savoir faire afin de réaliser le design le plus adapté à votre demande.</p>
+                    <p>Le développement est la partie technique, souvent la moins accessible pour nos clients. Nous nous efforçons de vulgariser au maximum afin d'être aux plus proches de nos clients, ainsi que leur apporter des connaissances.</p>
                 </div>
                 <div class="list">
                     <div class="elem" itemscope itemtype="https://schema.org/Thing">
                         <img itemprop="image" src="{{asset('images/svg/arrow-right-circle.svg')}}">
-                        <span itemprop="description">Réalisation de maquettes</span>
+                        <span itemprop="description">HTML, CSS, JavaScript, Php</span>
                     </div>
                     <div class="elem" itemscope itemtype="https://schema.org/Thing">
                         <img itemprop="image" src="{{asset('images/svg/arrow-right-circle.svg')}}">
-                        <span itemprop="description">Echange avec le client</span>
+                        <span itemprop="description">Test en temps réel, en ligne</span>
                     </div>
                     <div class="elem" itemscope itemtype="https://schema.org/Thing">
                         <img itemprop="image" src="{{asset('images/svg/arrow-right-circle.svg')}}">
-                        <span itemprop="description">Page par page</span>
+                        <span itemprop="description">Installation du site sur votre machine</span>
                     </div>
                 </div>
             </div>
