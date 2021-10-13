3248bbf088c6e85c4dc90135cfe832874e25559f
BuildTools <sarquentin@gmail.com>
2021-10-13 02:29:07 +0200

    Fix

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\DevisController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/mentions-legales', [MainController::class, 'legal'])->name('legal');
