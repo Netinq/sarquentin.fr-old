e8518827a062c124ff967f66501cd532edc8055b
BuildTools <sarquentin@gmail.com>
2021-10-13 02:31:11 +0200

    Fix

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\DevisController;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 
-URL::forceScheme('https');
-
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/mentions-legales', [MainController::class, 'legal'])->name('legal');
 Route::post('/sendemail/send', [DevisController::class, 'send'])->name('sendmail');
